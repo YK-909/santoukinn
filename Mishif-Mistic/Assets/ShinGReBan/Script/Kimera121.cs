5f0f9d863d748ee5b95c39357b50eee66ef0a16b
Takasu <m01191694a@edu.teu.ac.jp>
2021-04-18 00:12:25 +0900

    新しく追加。高須

---
+++ Mishif-Mistic/Assets/ShinGReBan/Script/Kimera121.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SelectCharacter
+{
+    public class Kimera121 : MonoBehaviour
+    {
+        [SerializeField]
+        int Head;
+        [SerializeField]
+        int Body;
+        [SerializeField]
+        int Leg;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Contlole.GetHead() == 1 && ContloleBody.GetBody() == 2 && ContloleLeg.GetLeg() == 1)
+            {
+                this.gameObject.SetActive(true);
+            }
+            else
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
