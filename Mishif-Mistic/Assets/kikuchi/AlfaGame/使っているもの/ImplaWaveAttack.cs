a65170b7760bc2e2b7288ef5f37c7ddda68ddbf2
grow409 <m011909923@edu.teu.ac.jp>
2021-05-11 00:29:01 +0900

    マップタイトル変更、マップ移動は確認したがキャラが表示されない

---
+++ Mishif-Mistic/Assets/kikuchi/AlfaGame/使っているもの/ImplaWaveAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImplaWaveAttack : MonoBehaviour
+{
+    public int playerID = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gard"))
+        {
+            this.tag = ("P" + playerID + "ImplaWaveBack");
+            Invoke("Impla2Normal", 1.0f);
+        }
+    }
+    void Impla2Normal()
+    {
+        this.tag = ("P" + playerID + "ImplaWave");
+    }
+}
---
+++ Mishif-Mistic/Assets/kikuchi/AlfaGame/使っているもの/LionAttack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LionAttack : MonoBehaviour
+{
+    public int playerID = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gard"))
+        {
+            this.tag = ("P"+ playerID +"LionAttackBack" );
+            Invoke("LionNormal", 1.0f);
+        }
+    }
+    void LionNormal()
+    {
+        this.tag = ("P"+playerID + "LionAttack");
+    }
+}
