4eb638103bd5b97d030da84ac2342b2c6fe2b794
masami-777 <m0119236b3@edu.teu.ac.jp>
2021-05-28 10:53:27 +0900

    Titleとパーツ選択のUIがADXで鳴るように設定。

---
+++ Mishif-Mistic/Assets/ShinGReBan/Script/ADX_SlotLR_CuePlay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADX_SlotLR_CuePlay : MonoBehaviour
+{
+    //ADX設定
+    private CriAtomSource atomSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CriAtomSourceを取得
+        atomSrc = (CriAtomSource)GetComponent("CriAtomSource");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左傾き1P
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //右傾きIP
+        else if (0 < Input.GetAxisRaw("Horizontal"))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //左2P
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //右2P
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+    }
+}
---
+++ Mishif-Mistic/Assets/ShinGReBan/Script/ADX_SlotUD_CuePlay.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADX_SlotUD_CuePlay : MonoBehaviour
+{
+    //ADX設定
+    private CriAtomSource atomSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CriAtomSourceを取得
+        atomSrc = (CriAtomSource)GetComponent("CriAtomSource");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左2P
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //右2P
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //上傾き
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+        //下傾き
+        else if (0 < Input.GetAxisRaw("Vertical"))
+        {
+            //音鳴らす
+            atomSrc.Play();
+        }
+    }
+}
