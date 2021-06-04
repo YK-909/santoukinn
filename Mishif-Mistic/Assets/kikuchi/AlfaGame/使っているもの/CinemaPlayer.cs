cdbc63fade4ca8b134e9b870188c1d82b978d001
YK-909 <m011909923@edu.teu.ac.jp>
2021-06-04 19:35:20 +0900

    gitで見追跡であったので追跡するようにした。

---
+++ Mishif-Mistic/Assets/kikuchi/AlfaGame/使っているもの/CinemaPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemaPlayer : MonoBehaviour
+{
+    public GameObject FSI;
+    public GameObject FSW;
+    public GameObject FTI;
+    public GameObject FTW;
+    public GameObject LSI;
+    public GameObject LSW;
+    public GameObject LTI;
+    public GameObject LTW;
+
+    private Vector3 PlayerObject;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FSI.activeSelf)
+        {
+            PlayerObject = FSI.transform.position;
+        }
+        else if (FSW.activeSelf)
+        {
+            PlayerObject = FSW.transform.position;
+        }
+        else if (FTI.activeSelf)
+        {
+            PlayerObject = FTI.transform.position;
+        }
+        else if (FTW.activeSelf)
+        {
+            PlayerObject = FTW.transform.position;
+        }
+        else if (LSI.activeSelf)
+        {
+            PlayerObject = LSI.transform.position;
+        }
+        else if (LSW.activeSelf)
+        {
+            PlayerObject = LSW.transform.position;
+        }
+        else if (LTI.activeSelf)
+        {
+            PlayerObject = LTI.transform.position;
+        }
+        else if (LTW.activeSelf)
+        {
+            PlayerObject = LTW.transform.position;
+        }
+        this.transform.position = PlayerObject;
+    }
+}
