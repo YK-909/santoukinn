2682561b148f5d08a61b9052270046b994f7ef1c
Masami <m0119236b3@edu.teu.ac.jp>
2021-07-06 16:03:17 +0900

    タイトルBGMが操作説明のシーンでも維持されるように設定。UIのキー操作での選択音も鳴るように設定。パーツ選択では鳴らないようになっている。

---
+++ Mishif-Mistic/Assets/Masami/Script/SelectBGMManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBGMManager : MonoBehaviour
+{
+    public string SelectBGMcueSheet = "SelectBGM";
+
+    private CriAtomSource atomSource;
+    static private SelectBGMManager instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(this);
+            return;
+        }
+
+        atomSource = gameObject.AddComponent<CriAtomSource>();
+        atomSource.cueSheet = SelectBGMcueSheet;
+
+        GameObject.DontDestroyOnLoad(this.gameObject);
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    static public void PlayCueId(int cueID)
+    {
+        instance.atomSource.Play(cueID);
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
+
